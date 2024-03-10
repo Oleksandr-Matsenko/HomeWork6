123194000f9239ca1cbfeeab27add09f9a3842c7
Oleksandr-Matsenko <olexandr282@gmail.com>
2024-03-10 11:48:20 +0200

    Add empty project

---
+++ HomeWork6/HomeWork6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork6
+//
+//  Created by Alex Matsenko on 10.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
